1f197c43e4b32d1b5afe59027f3f5d70991d641c
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-02-23 08:58:24 -0300

    :sparkles: feat(Section) classe imultavel Section
    classe freezed Section gerada e compilada

---
+++ lib/domain/models/section/section.dart
@@ -0,0 +1,20 @@
+import 'package:digprev_flutter/domain/models/enuns/typeCalculate.dart';
+import 'package:digprev_flutter/domain/models/question/question.dart';
+import 'package:digprev_flutter/domain/models/section/stageLabel.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'section.freezed.dart';
+part 'section.g.dart';
+
+@freezed
+class Section with _$Section{
+  const factory Section({
+    required int id,
+    required String title,
+    required int maxValue,
+    required TypeCalculate typeCalculate,
+    required List<StageLabel> textUnderBar,
+    required List<Question> questions,
+}) = _Section;
+  factory Section.fromJson(Map<String, dynamic> json) => _$SectionFromJson(json);
+}
---
+++ lib/domain/models/section/section.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'section.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SectionImpl _$$SectionImplFromJson(Map<String, dynamic> json) =>
+    _$SectionImpl(
+      id: (json['id'] as num).toInt(),
+      title: json['title'] as String,
+      maxValue: (json['maxValue'] as num).toInt(),
+      typeCalculate: $enumDecode(_$TypeCalculateEnumMap, json['typeCalculate']),
+      textUnderBar:
+          (json['textUnderBar'] as List<dynamic>)
+              .map((e) => StageLabel.fromJson(e as Map<String, dynamic>))
+              .toList(),
+      questions:
+          (json['questions'] as List<dynamic>)
+              .map((e) => Question.fromJson(e as Map<String, dynamic>))
+              .toList(),
+    );
+
+Map<String, dynamic> _$$SectionImplToJson(_$SectionImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'maxValue': instance.maxValue,
+      'typeCalculate': _$TypeCalculateEnumMap[instance.typeCalculate]!,
+      'textUnderBar': instance.textUnderBar,
+      'questions': instance.questions,
+    };
+
+const _$TypeCalculateEnumMap = {
+  TypeCalculate.PONTUACAO: 'PONTUACAO',
+  TypeCalculate.ATIVIDADE_FISICA: 'ATIVIDADE_FISICA',
+};
