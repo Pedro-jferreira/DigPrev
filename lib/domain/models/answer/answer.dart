f60e374f9d7928054b66497e53bdfb7e5b4bf66c
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-02-24 10:09:44 -0300

    :sparkles: feat(Answer) model Answer criado

---
+++ lib/domain/models/answer/answer.dart
@@ -0,0 +1,16 @@
+import 'package:digprev_flutter/domain/models/question/option.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'answer.freezed.dart';
+part 'answer.g.dart';
+
+@freezed
+class Answer with _$Answer {
+  const factory Answer({
+    required String id,
+    required DateTime date,
+    required List<Option> answers,
+  }) = _Answer;
+
+  factory Answer.fromJson(Map<String, dynamic> json) => _$AnswerFromJson(json);
+}
---
+++ lib/domain/models/answer/answer.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'answer.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AnswerImpl _$$AnswerImplFromJson(Map<String, dynamic> json) => _$AnswerImpl(
+  id: json['id'] as String,
+  date: DateTime.parse(json['date'] as String),
+  answers:
+      (json['answers'] as List<dynamic>)
+          .map((e) => Option.fromJson(e as Map<String, dynamic>))
+          .toList(),
+);
+
+Map<String, dynamic> _$$AnswerImplToJson(_$AnswerImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'date': instance.date.toIso8601String(),
+      'answers': instance.answers,
+    };
