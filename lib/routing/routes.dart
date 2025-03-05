a6e22af397741fed893149db98d4a406f0888256
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-03-04 22:19:04 -0300

    :sparkles: feat(routes)  ambiente de rotas criado

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,5 +1,4 @@
 class AppRoutes {
-
   static const AppRoute quiz = AppRoute(
     path: '/quiz',
     name: 'quiz',
@@ -9,6 +8,7 @@ class AppRoutes {
   static const AppRoute section = AppRoute(
     path: 'section/:id',
     name: 'section',
+    isScrollable: false,
   );
 
   //relatorios
@@ -17,21 +17,35 @@ class AppRoutes {
   // profile
   static const AppRoute profile = AppRoute(path: '/profile', name: 'profile');
 
+  static final List<AppRoute> allRoutes = <AppRoute>[
+    quiz,
+    section,
+    posts,
+    profile,
+  ];
 
-
-  static final List<AppRoute> allRoutes = <AppRoute>[quiz, section, posts, profile];
-
-
+  static bool isScrollableForName(String name) {
+    return allRoutes
+        .firstWhere(
+          (AppRoute route) => route.name == name,
+          orElse:
+              () =>
+                  const AppRoute(name: 'default', path: '', isScrollable: true),
+        )
+        .isScrollable;
+  }
 }
 
 class AppRoute {
   final String path;
   final String name;
+  final bool isScrollable;
   final List<AppRoute>? appRoutes;
 
   const AppRoute({
     required this.path,
     required this.name,
+    this.isScrollable = true,
     this.appRoutes,
   });
 }
--- lib/utils/navigatorContainerWithPageView.dart
+++ lib/utils/navigatorContainerWithPageView.dart
@@ -14,15 +14,19 @@ class NavigatorContainerWithPageView extends HookWidget {
   final StatefulNavigationShell navigationShell;
   final List<Widget> children;
 
+
   @override
   Widget build(BuildContext context) {
+
+    final String basePath = GoRouterState.of(context).topRoute!.name.toString();
+    print(basePath);
+    final bool isRouteBlocked = AppRoutes.isScrollableForName(basePath);
     final (:pageController, :onPageChanged) = usePageView(navigationShell);
     return PageView(
       controller: pageController,
-      physics:
-      GoRouter.of(context).canPop()
-          ? const NeverScrollableScrollPhysics()
-          : const ClampingScrollPhysics(),
+      physics: isRouteBlocked
+          ? const ClampingScrollPhysics()
+          : const NeverScrollableScrollPhysics(),
       onPageChanged: onPageChanged,
       children: children,
     );
