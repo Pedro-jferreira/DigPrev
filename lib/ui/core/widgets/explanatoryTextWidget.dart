3212abb83e0fafe1cdbcffe89ba92f84724c4575
arianamesquita <arianamesquita85@gmail.com>
2025-03-12 10:22:01 -0300

    feature(auth): criação do inputSlideWidget

--- lib/ui/core/widgets/explanatoryTextWidget.dart
+++ lib/ui/core/widgets/explanatoryTextWidget.dart
@@ -1,14 +1,16 @@
-import 'package:digprev_flutter/ui/core/widgets/titleToolTip.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class ExplanatoryTextWidget extends StatefulWidget {
-  final String labelText;
+  final String? labelText;
+  final int? valueMax;
+  final int? valueMin;
   final List<String> explanatoryText;
 
   const ExplanatoryTextWidget({
-    required this.labelText,
     required this.explanatoryText,
+    this.valueMax,
+    this.valueMin,
+    this.labelText,
     Key? key,
   });
 
@@ -22,22 +24,28 @@ class _ExplanatoryTextWidgetState extends State<ExplanatoryTextWidget> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(
-          widget.labelText,
-          selectionColor: Theme.of(context).colorScheme.onSurface,
-          style: Theme.of(
-            context,
-          ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w800),
-          softWrap: true,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-        ),
+        if (widget.labelText?.isNotEmpty ?? false) ...<Widget>[
+          Text(
+            widget.labelText!,
+            selectionColor: Theme.of(context).colorScheme.onSurface,
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.w800
+            ),
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+          const SizedBox(height: 8),
+        ],
         const SizedBox(height: 8),
-        ...widget.explanatoryText.asMap().entries.map((
-          MapEntry<int, String> entry,
-        ) {
-          final int index = entry.key + 1;
+        ...widget.explanatoryText.asMap().entries.map(
+                (MapEntry<int, String> entry) {
+          final bool isLast = entry.key == widget.explanatoryText.length - 1;
+          final int index = isLast && widget.valueMax != null
+              ? widget.valueMax!
+              : entry.key + 1;
           final String text = entry.value;
+
           return Padding(
             padding: const EdgeInsets.only(bottom: 4),
             child: Text(
