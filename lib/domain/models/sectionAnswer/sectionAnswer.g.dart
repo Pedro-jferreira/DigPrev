b7ec89611d42f39001170d94e5f84191942841e0
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-03-18 00:22:52 -0300

    :recycle: refactor(chart)

--- lib/domain/models/sectionAnswer/sectionAnswer.g.dart
+++ lib/domain/models/sectionAnswer/sectionAnswer.g.dart
@@ -10,7 +10,7 @@ _$SectionAnswerImpl _$$SectionAnswerImplFromJson(Map<String, dynamic> json) =>
     _$SectionAnswerImpl(
       id: json['id'] as String,
       title: json['title'] as String,
-      sectionRef: json['sectionRef'] as String,
+      sectionRef: (json['sectionRef'] as num).toDouble(),
       answers:
           (json['answers'] as List<dynamic>)
               .map((e) => Answer.fromJson(e as Map<String, dynamic>))
@@ -21,7 +21,7 @@ _$SectionAnswerImpl _$$SectionAnswerImplFromJson(Map<String, dynamic> json) =>
               .toList(),
       score: (json['score'] as num).toDouble(),
       isCalculated: json['isCalculated'] as bool,
-      hasChart: json['hasChart'] as bool?,
+      hasChart: json['hasChart'] as bool,
     );
 
 Map<String, dynamic> _$$SectionAnswerImplToJson(_$SectionAnswerImpl instance) =>
